9bff49b46f477a7644527ec7bbdafefa1ff1247c
dianlight <lucio.tarantino@gmail.com>
2020-02-16 14:18:28 +0100

    fix image push

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@
 for addon in "$@"; do
     if [ -z ${TRAVIS_COMMIT_RANGE} ] || git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep -v README.md | grep -q ${addon}; then
 		docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/${addon}:/data homeassistant/amd64-builder --all -t /data
+#		docker run --rm --privileged -v $(pwd)/${addon}:/data homeassistant/amd64-builder --all -t /data
     else
 	echo "No change in commit range ${TRAVIS_COMMIT_RANGE}"
     fi
