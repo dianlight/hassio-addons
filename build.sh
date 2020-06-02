dd3b5e4351b157292df0bec002b0fb56c5751ad1
dianlight <lucio.tarantino@gmail.com>
2020-06-02 19:38:13 +0200

    fix build

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
  
-#!/bin/bash -X
+#!/bin/bash
 
 for addon in "$@"; do
     if [ -z ${TRAVIS_COMMIT_RANGE} ] || git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep -v README.md | grep -q ${addon}; then
       echo $(pwd)/${addon}
-  		docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/${addon}:/data homeassistant/amd64-builder --all -t /data
+      for arch in "_armhf" "armv7" "_amd64" "_aarch64" "_i386" ; do
+       echo Target: ${arch}
+       grep ${arch} $(pwd)/${addon}/config.json | grep "\"arch\":" >/dev/null && \
+    		docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/${addon}:/data homeassistant/amd64-builder --${arch} -t /data \
+        || echo "Unsupported or Error!"
+      done 
     else
     	echo "No change in commit range ${TRAVIS_COMMIT_RANGE}"
     fi
