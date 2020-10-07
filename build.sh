af5b8a74da94015f2533d28cd1269e840fe1879e
dianlight <lucio.tarantino@gmail.com>
2020-10-07 16:33:47 +0200

    New version release

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@
 for addon in "$@"; do
     if [ -z ${TRAVIS_COMMIT_RANGE} ] || git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep -v README.md | grep -q ${addon}; then
       echo $(pwd)/${addon}
-      for arch in "_armhf" "armv7" "_amd64" "_aarch64" "_i386" ; do
+      for arch in "armhf" "armv7" "amd64" "aarch64" "i386" ; do
        echo Target: ${arch}
        grep ${arch} $(pwd)/${addon}/config.json | grep "\"arch\":" >/dev/null && \
-    		docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/${addon}:/data homeassistant/amd64-builder --${arch} -t /data \
+    		docker run  --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/${addon}:/data homeassistant/amd64-builder --docker-hub-check --${arch} -t /data \
         || echo "Unsupported or Error!"
       done 
     else
