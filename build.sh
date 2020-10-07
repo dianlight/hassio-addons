934d0534ab6ee94ddcb2cbac38bf84cee2241905
dianlight <lucio.tarantino@gmail.com>
2020-10-07 22:05:01 +0200

    Try to correct upload to dockerhub.

--- build.sh
+++ build.sh
@@ -11,5 +11,5 @@ for addon in "$@"; do
     archs=$(jq -r '.arch // ["armv7", "armhf", "amd64", "aarch64", "i386"] | [.[] | "--" + .] | join(" ")' ${addon}/config.json)
     echo "${ANSI_GREEN}Building ${addon} -> ${archs} ${ANSI_CLEAR}"
 #    docker run  --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/${addon}:/data homeassistant/amd64-builder --docker-hub-check --${arch} -t /data 
-    docker run  --rm --privileged -v '/var/run/docker.sock:/var/run/docker.sock' -v $(pwd)/${addon}:/data homeassistant/amd64-builder --docker-hub-check ${archs} -t /data 
+    docker run  --rm --privileged -v ~/.docker:/root/.docker -v '/var/run/docker.sock:/var/run/docker.sock' -v $(pwd)/${addon}:/data homeassistant/amd64-builder --docker-hub-check ${archs} -t /data 
 done
