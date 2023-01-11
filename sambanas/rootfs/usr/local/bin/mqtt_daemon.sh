6fa436a3a4adb587204bb63efba50bd4ea43aad3
dianlight <lucio.tarantino@gmail.com>
2023-01-11 19:52:23 +0100

    Refactor part2

--- sambanas/rootfs/usr/local/bin/mqtt_daemon.sh
+++ sambanas/rootfs/usr/local/bin/mqtt_daemon.sh
@@ -4,43 +4,63 @@ declare moredisks
 declare status
 declare fsdata
 
-topic=$(bashio::config 'mqtt_topic');if [ "$topic" = "null" ]; then topic="sambanas"; fi;
+topic=$(bashio::config 'mqtt_topic')
+if [ "$topic" = "null" ]; then topic="sambanas"; fi
 
-# Home assistant auto discovery    
+# Home assistant auto discovery
 if [ -f /root/.config/mosquitto_pub ] && ! bashio::config.true "autodiscovery.disable_discovery"; then
     a=({a..z})
-    if ! bashio::config.true "autodiscovery.disable_persistent"; then prs="-r";fi
+    if ! bashio::config.true "autodiscovery.disable_persistent"; then prs="-r"; fi
     bashio::log.info "Sending MQTT autodiscovery..."
-    device="\"device\":{\"identifiers\":[\"${topic}\"],\"name\": \"Samba Nas Sensors\", \"model\": \"Samba $(smbd -V)\", \"manufacturer\": \"@Dianlight\"},\"icon\":\"mdi:harddisk\"" 
-    for disk in $(awk '/^   path = .*/g { print $3 }' /etc/samba/smb.conf) 
-    do
+    device="\"device\":{\"identifiers\":[\"${topic}\"],\"name\": \"Samba Nas Sensors\", \"model\": \"Samba $(smbd -V)\", \"manufacturer\": \"@Dianlight\"},\"icon\":\"mdi:harddisk\""
+    for disk in $(awk '/^   path = .*/g { print $3 }' /etc/samba/smb.conf); do
         ldisk=${disk##*/}
         mosquitto_pub ${prs} -t "homeassistant/sensor/${topic}/${a}_size/config" -m \
-        "{\"name\": \"${topic} Size ${disk}\", \"unit_of_measurement\": \"Kb\",\"unique_id\":\"${topic}_size_${ldisk,,}\",${device},\"value_template\": \"{{ value_json.size_${ldisk,,}}}\", \"state_topic\": \"homeassistant/sensor/${topic}/state\"}"
+            "{\"name\": \"${topic} Size ${disk}\", \"unit_of_measurement\": \"Kb\",\"unique_id\":\"${topic}_size_${ldisk,,}\",${device},\"value_template\": \"{{ value_json.size_${ldisk,,}}}\", \"state_topic\": \"homeassistant/sensor/${topic}/state\"}"
         mosquitto_pub ${prs} -t "homeassistant/sensor/${topic}/${a}_used/config" -m \
-        "{\"name\": \"${topic} Used ${disk}\", \"unit_of_measurement\": \"Kb\",\"unique_id\":\"${topic}_used_${ldisk,,}\",${device}, \"value_template\": \"{{ value_json.used_${ldisk,,}}}\", \"state_topic\": \"homeassistant/sensor/${topic}/state\"}"
+            "{\"name\": \"${topic} Used ${disk}\", \"unit_of_measurement\": \"Kb\",\"unique_id\":\"${topic}_used_${ldisk,,}\",${device}, \"value_template\": \"{{ value_json.used_${ldisk,,}}}\", \"state_topic\": \"homeassistant/sensor/${topic}/state\"}"
         mosquitto_pub ${prs} -t "homeassistant/sensor/${topic}/${a}_available/config" -m \
-        "{\"name\": \"${topic} Available ${disk}\", \"unit_of_measurement\": \"Kb\",\"unique_id\":\"${topic}_available_${ldisk,,}\",${device}, \"value_template\": \"{{ value_json.available_${ldisk,,}}}\", \"state_topic\": \"homeassistant/sensor/${topic}/state\"}"
+            "{\"name\": \"${topic} Available ${disk}\", \"unit_of_measurement\": \"Kb\",\"unique_id\":\"${topic}_available_${ldisk,,}\",${device}, \"value_template\": \"{{ value_json.available_${ldisk,,}}}\", \"state_topic\": \"homeassistant/sensor/${topic}/state\"}"
         mosquitto_pub ${prs} -t "homeassistant/sensor/${topic}/${a}_use/config" -m \
-        "{\"name\": \"${topic} Use% ${disk}\", \"unit_of_measurement\": \"%\",\"unique_id\":\"${topic}_use_${ldisk,,}\",${device}, \"value_template\": \"{{ value_json.use_${ldisk,,}}}\", \"state_topic\": \"homeassistant/sensor/${topic}/state\"}"
+            "{\"name\": \"${topic} Use% ${disk}\", \"unit_of_measurement\": \"%\",\"unique_id\":\"${topic}_use_${ldisk,,}\",${device}, \"value_template\": \"{{ value_json.use_${ldisk,,}}}\", \"state_topic\": \"homeassistant/sensor/${topic}/state\"}"
+
+        #        mosquitto_pub ${prs} -t "homeassistant/sensor/${topic}/${a}_type/config" -m \
+        #            "{\"name\": \"${topic} FSType ${disk}\", \"unique_id\":\"${topic}_fstype_${ldisk,,}\",${device},\"value_template\": \"{{ value_json.${ldisk,,}.fstype}}\", \"state_topic\": \"homeassistant/sensor/${topic}/state\"}"
+        #        mosquitto_pub ${prs} -t "homeassistant/sensor/${topic}/${a}_size/config" -m \
+        #            "{\"name\": \"${topic} Size ${disk}\", \"unit_of_measurement\": \"b\",\"unique_id\":\"${topic}_size_${ldisk,,}\",${device},\"value_template\": \"{{ value_json.${ldisk,,}.fssize}}\", \"state_topic\": \"homeassistant/sensor/${topic}/state\"}"
+        #        mosquitto_pub ${prs} -t "homeassistant/sensor/${topic}/${a}_used/config" -m \
+        #            "{\"name\": \"${topic} Used ${disk}\", \"unit_of_measurement\": \"b\",\"unique_id\":\"${topic}_used_${ldisk,,}\",${device}, \"value_template\": \"{{ value_json.${ldisk,,}.fsused}}\", \"state_topic\": \"homeassistant/sensor/${topic}/state\"}"
+        #        mosquitto_pub ${prs} -t "homeassistant/sensor/${topic}/${a}_available/config" -m \
+        #            "{\"name\": \"${topic} Available ${disk}\", \"unit_of_measurement\": \"b\",\"unique_id\":\"${topic}_available_${ldisk,,}\",${device}, \"value_template\": \"{{ value_json.${ldisk,,}.fsavail}}\", \"state_topic\": \"homeassistant/sensor/${topic}/state\"}"
+        #        mosquitto_pub ${prs} -t "homeassistant/sensor/${topic}/${a}_use/config" -m \
+        #            "{\"name\": \"${topic} Use% ${disk}\", \"unit_of_measurement\": \"%\",\"unique_id\":\"${topic}_use_${ldisk,,}\",${device}, \"value_template\": \"{{ value_json.${ldisk,,}.fsuse%}}\", \"state_topic\": \"homeassistant/sensor/${topic}/state\"}"
         a=("${a[@]:1}")
     done
 fi
 
+#disks=$(awk 'BEGIN { ORS=""; print "["}  /^   path = .*/g {  printf "%s\"%s\"",separator,$3 ; separator=","  } END { print "]" } ' /etc/samba/smb.conf)
+#blk=$(lsblk -b -no NAME,LABEL,FSSIZE,FSUSED,FSUSE%,FSAVAIL,FSTYPE,MOUNTPOINTS -J)
+### New based on real block devices
+#echo ${blk} | jq --argjson disks $disks 'reduce (.blockdevices[].children[]? |select(.mountpoints? - $disks != .mountpoints) ) as $i ({};.[$i.label // $i.name] = $i) '
+
+## OLD based on share
 while true; do
     # Create status message
-    status="{\"1\":\"1\"";
-    for disk in $(awk '/^   path = .*/g { print $3 }' /etc/samba/smb.conf)
-    do
+    blk=$(lsblk -b -no FSSIZE,FSUSED,FSUSE%,FSAVAIL,FSTYPE,MOUNTPOINTS -J)
+
+    status="{}"
+    for disk in $(awk '/^   path = .*/g { print $3 }' /etc/samba/smb.conf); do
         ldisk=${disk##*/}
-        mapfile -t fsdata < <(df $disk)
-        fsd=(${fsdata[1]})
-        status="$status, \"size_${ldisk,,}\":\"${fsd[1]}\""
-        status="$status, \"used_${ldisk,,}\":\"${fsd[2]}\""
-        status="$status, \"available_${ldisk,,}\":\"${fsd[3]}\""
-        status="$status, \"use_${ldisk,,}\":\"${fsd[4]%?}\""
-    done    
-    status="$status}"
+        #        mapfile -t fsdata < <(lsblk -b -no FSSIZE,FSUSED,FSUSE%,FSAVAIL,FSTYPE,MOUNTPOINT,MODEL | grep $disk)
+        #        fsd=(${fsdata[0]})
+        #        status="$status, \"fstype_${ldisk,,}\":\"${fsd[4]}\""
+        #        status="$status, \"size_${ldisk,,}\":\"${fsd[0]}\""
+        #        status="$status, \"used_${ldisk,,}\":\"${fsd[1]}\""
+        #        status="$status, \"available_${ldisk,,}\":\"${fsd[3]}\""
+        #        status="$status, \"use_${ldisk,,}\":\"${fsd[2]}\""
+        json=$(echo ${blk} | jq --arg disk "$disk" '.blockdevices[]| select(.mountpoints?|index($disk))')
+        status=$(echo $status $json | jq --arg ldisk "${ldisk,,}" -s '.[0] + (.[1]|{("size_"+$ldisk):(.fssize / 1024),("used_"+$ldisk):(.fsused/1024),("available_"+$ldisk):(.fsavail/1024),("use_"+$ldisk):."fsuse%"|rtrimstr("%")})')
+    done
     # Send status message
     mosquitto_pub -t "homeassistant/sensor/${topic}/state" -m "$status"
     # Sleep
