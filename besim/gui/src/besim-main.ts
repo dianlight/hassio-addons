1784ffa8bd4b7dead5bcf9745823ebe5429bf901
dianlight <lucio.tarantino@gmail.com>
2024-02-25 22:54:33 +0100

    ✨ feat (webgui): First version of API call log

--- besim/gui/src/besim-main.ts
+++ besim/gui/src/besim-main.ts
@@ -1,88 +1,78 @@
 //import "./devices/devices-list";
 //import "./components/esphome-header-menu";
 //import "./components/esphome-fab";
-import { LitElement, html, css /*, PropertyValues*/ } from "lit";
+import { LitElement, html /*, PropertyValues*/ } from "lit";
 import { customElement, property, queryAll, state } from "lit/decorators.js";
 import './device/device-list'
 import './traffic/traffic-table'
-import './unknown/udp-table'
-import './unknown/api-table'
-import '@material/typography/dist/mdc.typography.css'
-
-
 
 @customElement("besim-main")
 class BeSimMainView extends LitElement {
-  @property() accessor version = "unknown";
+  @property() version = "unknown";
 
-  @property() accessor token!: string;
+  @property() token?: string;
 
-  /*
-  @property() accessor docsLink = "";
+  @property() docsLink = "";
 
-  @property() accessor logoutUrl!: string;
-  */
+  @property() logoutUrl?: string;
 
-  @state() accessor activeTabIndex!: string;
+  @state() private activeTabIndex?: string;
 
-  @property() accessor deviceIds: string[] = [];
+  @property() deviceIds: string[] = [];
 
-  @queryAll("div[id$='-panel'")
-  accessor _panels!: NodeListOf<HTMLDivElement>;
-
-  static get styles() {
-    return css`
-      @use "@material/typography/mdc-typography";
-    `
-  }
+  @queryAll("div[id$='-panel'") _panels?: NodeListOf<HTMLDivElement>;
 
   protected render() {
     return html`
       <header class="besim-header">
-        <img src="assets/images/logo.png" alt="BeSim Logo"/>
+        <img src="static/images/logo-text.svg" alt="BeSim Logo -${this.activeTabIndex ?? 0}"/>
         <div class="flex"></div>
+        <!--
+        <esphome-header-menu .logoutUrl=${this.logoutUrl}></esphome-header-menu>
+        -->
         <md-tabs aria-label="Content to view" @change="${this._changeTab}" .activeTabIndex=${this.activeTabIndex ?? 0}>
           <md-primary-tab id="devices-tab" aria-controls="devices-panel">Devices</md-primary-tab>
-          <md-primary-tab id="traffic-tab" aria-controls="traffic-panel">Call Log</md-primary-tab>
-          <md-primary-tab id="missing-api-tab" aria-controls="missing-api-panel">Missing API</md-primary-tab>
+          <md-primary-tab id="traffic-tab" aria-controls="traffic-panel">Api</md-primary-tab>
+          <md-primary-tab id="status-tab" aria-controls="status-panel">Status</md-primary-tab>
         </md-tabs>
       </header>
 
       <main>
         <div id="devices-panel" role="tabpanel" aria-labelledby="devices-tab">
-          <h1 class="mdc-typography--headline1">Devices</h1>
           <device-list/>
         </div>
         <div id="traffic-panel" role="tabpanel" aria-labelledby="traffic-tab" hidden>
-          <h1 >Traffic</h1>
           <traffic-table/>
         </div>
-        <div id="missing-api-panel" role="tabpanel" aria-labelledby="missing-api-tab" hidden>
-          <h1>Missing API</h1>
-
-          <h2>Missing REST API</h2>
-          <api-unknown-table></api-unknown-table>
-
-          <h2>Missing UDP API</h2>
-          <udp-unknown-table></udp-unknown-table>
-        </div>
+        <div id="status-panel" role="tabpanel" aria-labelledby="status-tab" hidden>
+          ...
+        </div>      
       </main>
+
+      <!--
+      <esphome-fab></esphome-fab>
+      -->
 
       <footer class="page-footer">
         <div>
-          ${process.env.SERVER} |
           BeSIM by @Dianlight |
-          <a href="url: https://github.com/dianlight/hassio-addons" target="_blank"
+          <a href="https://esphome.io/guides/supporters.html" target="_blank"
             >Fund development</a
           >
           |
-          <a href="url: https://github.com/dianlight/hassio-addons/tree/master/besim" target="_blank" rel="noreferrer"
+          <a href=${this.docsLink} target="_blank" rel="noreferrer"
             >${this.version} Documentation</a
           >
         </div>
       </footer>
     `;
   }
+
+  /*
+  createRenderRoot() {
+    return this;
+  }
+  */
 
   _changeTab(e: Event) {
     this.activeTabIndex = e.target!['activeTabIndex'];
@@ -96,6 +86,7 @@ class BeSimMainView extends LitElement {
         panel.style.display = 'none'
       }
     });
+
   }
 
 
