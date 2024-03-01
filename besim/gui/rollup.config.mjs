7594da9d9fc5a0cbb6131ccc6c21972cea60910f
dianlight <lucio.tarantino@gmail.com>
2024-03-01 08:17:07 +0100

    🏗️ build (webgui): Fix path resolve map

--- besim/gui/rollup.config.mjs
+++ besim/gui/rollup.config.mjs
@@ -10,11 +10,11 @@ import commonjs from "@rollup/plugin-commonjs";
 import copy from "rollup-plugin-copy";
 //import fs from "fs-extra";
 //import path from "path";
-import serve from 'rollup-plugin-serve'
-import html from "@rollup/plugin-html";
 import livereload from 'rollup-plugin-livereload'
 import replace from "@rollup/plugin-replace";
 import dev from 'rollup-plugin-dev'
+import { mdIcon, replaceSymbolsLink } from 'rollup-plugin-md-icon';
+import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
 //import { compileLitTemplates } from '@lit-labs/compiler'
 
 const isProdBuild = process.env.NODE_ENV === "production";
@@ -34,7 +34,6 @@ const config = {
     },
     preserveEntrySignatures: false,
     plugins: [
-        (!isProdBuild) && html(),
         replace({
             values: {
                 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
@@ -70,7 +69,7 @@ const config = {
         */
         copy({
             targets: [
-                { src: "assets/*", dest: "dist" },
+                { src: "assets/*", dest: "dist/assets" },
             ],
         }),
         /*
@@ -86,9 +85,22 @@ const config = {
         commonjs(),
         //json(),
         //manifest(),
-        //(!isProdBuild) && serve('dist'),
+        mdIcon({ symbols: {} }),
+        html({
+            input: ['src/index.html'],
+            publicPath: '/static',
+            //extractAssets: false,
+            /*
+            transformHtml: (html) =>
+                replaceSymbolsLink(
+                    html,
+                    '<link rel="stylesheet" href="/material-symbols.css">',
+                ),
+            */
+        }),
         dev({
-            dirs: ['dist'],
+            dirs: ['dist'/*, 'assets'*/],
+            basePath: '/static',
             proxy: [{
                 from: '/api',
                 to: 'http://127.0.0.1/api',
@@ -96,13 +108,15 @@ const config = {
                     logger: true,
                 }
             },
-            {
-                from: '/static',
-                to: 'http://127.0.0.1/static',
-                opts: {
-                    logger: true,
+                /*
+                {
+                    from: '/static',
+                    to: 'http://127.0.0.1/static',
+                    opts: {
+                        logger: true,
+                    }
                 }
-            }
+                */
             ]
         }),
         (!isProdBuild) && livereload('dist'),
